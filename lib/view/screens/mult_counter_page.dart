bd8b25bfed4b68c4f13f2ba849146976759080f7
Ahmed Eid Hefny <ahmedeid2026@gmail.com>
2022-04-24 22:22:02 +0200

    [NEW] GetX 2- State Management multi Obx ON PAGE

--- lib/view/screens/mult_counter_page.dart
+++ lib/view/screens/mult_counter_page.dart
@@ -4,7 +4,8 @@ import 'package:get_x_ecosystem_fluter_master/controller/sec_controller.dart';
 import 'package:get_x_ecosystem_fluter_master/controller/third_controller.dart';
 
 class MultiCounterSumPage extends StatelessWidget {
-  const MultiCounterSumPage({Key? key}) : super(key: key);
+  
+  var controller = Get.put(ThirdController());
 
   @override
   Widget build(BuildContext context) {
@@ -14,40 +15,38 @@ class MultiCounterSumPage extends StatelessWidget {
         child: Container(
           width: double.infinity,
           height: double.infinity,
-          child: GetX<ThirdController>(
-            init: ThirdController(),
-            builder: (controller) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'Count1 = ${controller.count1}',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Count2 = ${controller.count2}',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Sum = ${controller.sum()}',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      controller.increament1();
-                    },
-                    child: Icon(Icons.add),
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      controller.increament2();
-                    },
-                    child: Icon(Icons.add),
-                  )
-                ],
-              );
-            },
-          ),
+          child: Obx(() {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Count1 = ${controller.count1}',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Count2 = ${controller.count2}',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Sum = ${controller.sum()}',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    controller.increament1();
+                  },
+                  child: Icon(Icons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    controller.increament2();
+                  },
+                  child: Icon(Icons.add),
+                )
+              ],
+            );
+          }
+          )
         ),
       ),
     );
