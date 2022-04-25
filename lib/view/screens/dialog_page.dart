8850b858ca3433ad72a9684d8469af96515e63ba
Ahmed Eid Hefny <ahmedeid2026@gmail.com>
2022-04-25 07:03:33 +0200

    [NEW] GetX 6- Buttom Sheet

--- lib/view/screens/dialog_page.dart
+++ lib/view/screens/dialog_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_x_ecosystem_fluter_master/controller/counter_controller.dart';
@@ -18,25 +20,45 @@ class DialogPage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
-                Get.defaultDialog(
-                  title: "title",
-                  titlePadding: EdgeInsets.all(30.0),
-                  titleStyle: TextStyle(
-                    fontSize: 50,
+                // Get.defaultDialog(
+                //   title: "title",
+                //   titlePadding: EdgeInsets.all(30.0),
+                //   titleStyle: TextStyle(
+                //     fontSize: 50,
+                //   ),
+                //   backgroundColor: Colors.red,
+                //   textCancel: 'cancel',
+                //   textConfirm: 'confirm',
+                //   cancel: Text('cancel'),
+                //   content: Container(
+                //     width: 100.0,
+                //     height: 100.0,
+                //     color: Colors.blue,
+                //   ),
+                //   onCancel: (){
+                //   },
+                //   onConfirm: (){},
+                //   barrierDismissible: false
+                // );
+                Get.bottomSheet(
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(onPressed: (){}, child: Text('btn1')),
+                      ElevatedButton(onPressed: (){}, child: Text('btn2')),
+                      ElevatedButton(onPressed: (){}, child: Text('btn3')),
+                      ElevatedButton(onPressed: (){}, child: Text('btn4')),
+                    ],
                   ),
-                  backgroundColor: Colors.red,
-                  textCancel: 'cancel',
-                  textConfirm: 'confirm',
-                  cancel: Text('cancel'),
-                  content: Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.blue,
+                  isScrollControlled: true,
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0))
                   ),
-                  onCancel: (){
-                  },
-                  onConfirm: (){},
-                  barrierDismissible: false
+                  elevation: 10,
+                  enterBottomSheetDuration: Duration(seconds: 2),
+                  exitBottomSheetDuration: Duration(seconds: 3),
+                  isDismissible: false,
                 );
               },
               child: Text('show Dialog'),
